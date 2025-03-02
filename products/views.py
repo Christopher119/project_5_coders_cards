200e5d21e45bd6d79422baa430bbe785c939e212
Christopher119 <christopherdunne@outlook.ie>
2025-03-02 03:59:24 +0000

    Fix: typo in context preventing product detail from displaying correctly

--- products/views.py
+++ products/views.py
@@ -17,12 +17,12 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """ A view to show a product's details"""
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
-        'products': product,
+        'product': product,
     }
 
     return render(request, 'products/product_detail.html', context)
