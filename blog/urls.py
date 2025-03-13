420caa8c5f3c7f8e5399fc34ff5dec09ec125acd
Christopher119 <christopherdunne@outlook.ie>
2025-03-13 06:15:32 +0000

    Add: delete blog post functionality

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.blog, name='blog'),
     path('<slug:slug>/', views.blog_post, name='blog_post'),
     path('edit/<slug:slug>/', views.edit_blog_post, name='edit_blog_post'),
+    path('delete/<slug:slug>/', views.delete_blog_post, name='delete_blog_post'),
 ]
