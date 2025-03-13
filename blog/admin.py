c30b029da6378c2013a37144b304b8549d36f712
Christopher119 <christopherdunne@outlook.ie>
2025-03-13 00:29:56 +0000

    Add: custom 404 page view, url, and template

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import BlogPost, Comment
 
 
-# registering a class of BookAdmin
+# registering a class of BlogAdmin
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
 
